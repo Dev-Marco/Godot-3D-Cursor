989876f7f0170e0c700070f16ba77dd9f167b345
Dev-Marco <marco.koennecke@t-online.de>
2025-09-10 16:23:35 +0200

    Bumped version to 1.3.2

--- addons/godot_3d_cursor/plugin.gd
+++ addons/godot_3d_cursor/plugin.gd
@@ -152,7 +152,7 @@ func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_SHIFT) and Input.is_action_just_pressed("3d_cursor_set_location"):
 		mouse_position = editor_viewport.get_mouse_position()
 		_get_selection()
-		
+
 	if cursor == null or not cursor.is_inside_tree():
 		return
 
